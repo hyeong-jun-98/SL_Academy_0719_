b47581727670e6d000056cf1034157df6f3b5cb9
Yang <bellehee98@naver.com>
2022-07-20 16:52:01 +0900

    7/20 UP&DOWN

--- SL_Academy/SL_0720/src/Play.java
+++ SL_Academy/SL_0720/src/Play.java
@@ -1,101 +1,30 @@
-//
-//class Mall {
-//    String name;
-//    int price;
-//
-//    Mall() {
-//
-//    }
-//
-//    public void print() {
-//        System.out.println("name : " + name);
-//        System.out.println("price : " + price);
-//    }
-//}
-//
-//class Book extends Mall {
-//    String writer;
-//
-//    Book() {
-//    }
-//
-//    @Override
-//    public void print() {
-//        super.print();
-//        System.out.println("writer : " + writer);
-//    }
-//}
-//
-//
-//class Yogurt extends Mall {
-//    String expired;
-//
-//    Yogurt() {
-//
-//    }
-//
-//    @Override
-//    public void print() {
-//        super.print();
-//        System.out.println("expired : " + expired);
-//    }
-//}
-//
-//class Cup extends Mall {
-//    String size;
-//
-//    Cup() {
-//
-//    }
-//
-//    @Override
-//    public void print() {
-//        super.print();
-//        System.out.println("size : " + size);
-//    }
-//}
-//class Scanner extends Mall{
-//    int weight;
-//
-//
-//
-//    public  Scanner(String name, int price, int weight) {
-//        this.name = name;
-//        this.price = price;
-//        this.weight = weight;
-//    }
-//
-//    @Override
-//    public void print() {
-//        super.print();
-//        System.out.println("weight : " + weight);
-//    }
-//}
-
+import java.util.Random;
+import java.util.Scanner;
 
 public class Play {
     public static void main(String[] args) {
-//        Book book = new Book();
-//        Yogurt yogurt = new Yogurt();
-//        Cup cup = new Cup();
-//
-//        book.name = "삼국지";
-//        book.price = 10000;
-//        book.writer = "야관문";
-//        book.print();
-//        System.out.println("===============================");
-//        yogurt.name = "엔요";
-//        yogurt.price = 1000;
-//        yogurt.expired = "2022-08-10";
-//        yogurt.print();
-//        System.out.println("===============================");
-//        cup.name = "가나다컵";
-//        cup.price = 300;
-//        cup.size = "tall";
-//        cup.print();
-//        System.out.println("===============================");
-//
-//        Scanner scanner = new Scanner("캐논", 80000, 10 );
-//        scanner.print();
+        Scanner sc = new Scanner(System.in);
+        Random rd = new Random();
+        int count = 0;
+        int num = rd.nextInt(100) + 1;
+        //       int num = 43;
+        int input;
+
+        while (true) {
+            System.out.print(" 숫자를 입력하세요 : ");
+            input = sc.nextInt();
+            count++;
+            if (input < num) {
+                System.out.println("숫자가 작습니다 좀 더 큰 숫자를 입력하세요");
+            } else if (num == input) {
+                System.out.println();
+                System.out.println("** 정답!!  " + count + "번 만에 정답입니다. **");
+                break;
+            } else {
+                System.out.println("숫자가 큽니다. 좀 더 작은 숫자를 입력하세요");
+            }
+
+        }
+
     }
 }
