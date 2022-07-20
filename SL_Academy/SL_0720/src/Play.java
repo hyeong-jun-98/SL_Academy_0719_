3f614945a02c77b35b7e17cd061946b53b63a9e5
Yang <bellehee98@naver.com>
2022-07-20 15:48:26 +0900

    7/20 게터새터 생성자 멤버변수

---
+++ SL_Academy/SL_0720/src/Play.java
@@ -0,0 +1,101 @@
+//
+//class Mall {
+//    String name;
+//    int price;
+//
+//    Mall() {
+//
+//    }
+//
+//    public void print() {
+//        System.out.println("name : " + name);
+//        System.out.println("price : " + price);
+//    }
+//}
+//
+//class Book extends Mall {
+//    String writer;
+//
+//    Book() {
+//    }
+//
+//    @Override
+//    public void print() {
+//        super.print();
+//        System.out.println("writer : " + writer);
+//    }
+//}
+//
+//
+//class Yogurt extends Mall {
+//    String expired;
+//
+//    Yogurt() {
+//
+//    }
+//
+//    @Override
+//    public void print() {
+//        super.print();
+//        System.out.println("expired : " + expired);
+//    }
+//}
+//
+//class Cup extends Mall {
+//    String size;
+//
+//    Cup() {
+//
+//    }
+//
+//    @Override
+//    public void print() {
+//        super.print();
+//        System.out.println("size : " + size);
+//    }
+//}
+//class Scanner extends Mall{
+//    int weight;
+//
+//
+//
+//    public  Scanner(String name, int price, int weight) {
+//        this.name = name;
+//        this.price = price;
+//        this.weight = weight;
+//    }
+//
+//    @Override
+//    public void print() {
+//        super.print();
+//        System.out.println("weight : " + weight);
+//    }
+//}
+
+
+public class Play {
+    public static void main(String[] args) {
+//        Book book = new Book();
+//        Yogurt yogurt = new Yogurt();
+//        Cup cup = new Cup();
+//
+//        book.name = "삼국지";
+//        book.price = 10000;
+//        book.writer = "야관문";
+//        book.print();
+//        System.out.println("===============================");
+//        yogurt.name = "엔요";
+//        yogurt.price = 1000;
+//        yogurt.expired = "2022-08-10";
+//        yogurt.print();
+//        System.out.println("===============================");
+//        cup.name = "가나다컵";
+//        cup.price = 300;
+//        cup.size = "tall";
+//        cup.print();
+//        System.out.println("===============================");
+//
+//        Scanner scanner = new Scanner("캐논", 80000, 10 );
+//        scanner.print();
+    }
+}
---
+++ SL_Academy/SL_0720/src/com/SL_0720/main/Pen.java
@@ -0,0 +1,60 @@
+package com.SL_0720.main;
+
+ class Pen {
+//    String name;
+//    int price;
+//
+//    public Pen(String name, int price) {
+//        this.name = name;
+//        this.price = price;
+//    }
+//
+//    public void print() {
+//        System.out.println(name);
+//        System.out.println(price);
+//    }
+}
+
+
+class Computer {
+     // 항상 변수설정을 (멤버변수)할 때 private으로 설정한다.
+    private String name;
+    private int ram;
+    private int hdd;
+
+    // DTO javaBEAM
+
+    public Computer (){
+
+    }
+
+    public Computer(String name, int ram, int hdd) {
+        this.name = name;
+        this.ram = ram;
+        this.hdd = hdd;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public int getHdd() {
+        return hdd;
+    }
+
+    public void setHdd(int hdd) {
+        this.hdd = hdd;
+    }
+}
