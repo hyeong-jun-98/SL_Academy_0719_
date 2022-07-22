0620ff8821b4bd3b5b7bab6ba51bd0cf6f66db73
Yang <bellehee98@naver.com>
2022-07-22 17:56:23 +0900

    7/22  369 게임 하다가 중단

---
+++ SL_Academy/SL_0722/src/Practice2.java
@@ -0,0 +1,37 @@
+
+//  369게임
+public class Practice2 {
+    public static void main(String[] args) {
+        String[] arr = new String[35];
+
+        int count = 0;
+        // [1,2,3,*** 20]
+        //
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (i + "");
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length(); j++) {
+                if (arr[i].charAt(j) == '3' || arr[i].charAt(j) == '6' || arr[i].charAt(j) == '9') {
+                    arr[i] = "짝";
+                    System.out.print(arr[i]);
+                }
+
+            }
+
+        }
+
+
+//         if(arr[j].charAt(i) == 3 || arr[j].contains("6") || arr[j].contains("9")) {
+//        count = 1;
+//        arr[i] = "짝";
+//        System.out.println("짝");
+//        if(count == 2) {
+//            System.out.println("짝짝");
+//            arr[i] = "짝짝";
+//        }}
+
+
+    }
+}
