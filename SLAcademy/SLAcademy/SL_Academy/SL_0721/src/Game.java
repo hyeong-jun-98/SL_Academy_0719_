e17c8f454bbaeded65aebfc4ccf78351b29a1e77
Yang <bellehee98@naver.com>
2022-08-18 12:57:14 +0900

    22/07/21 컴퓨터랑 가위바위보

--- SLAcademy/SLAcademy/SL_Academy/SL_0721/src/Game.java
+++ SLAcademy/SLAcademy/SL_Academy/SL_0721/src/Game.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class Game {
+public class Game {     //
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random rd = new Random();
