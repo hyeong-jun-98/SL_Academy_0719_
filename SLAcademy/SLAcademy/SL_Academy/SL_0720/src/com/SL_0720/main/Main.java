66e4c7bbd17d252d7f7c71edd1d6a5e465251d95
Yang <bellehee98@naver.com>
2022-08-18 12:54:56 +0900

    22/07/20 게터세터 연습

--- SLAcademy/SLAcademy/SL_Academy/SL_0720/src/com/SL_0720/main/Main.java
+++ SLAcademy/SLAcademy/SL_Academy/SL_0720/src/com/SL_0720/main/Main.java
@@ -2,7 +2,7 @@ package com.SL_0720.main;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main { //
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
