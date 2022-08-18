0ca6f21cf160c27ef816ea275a784a1291c20655
Yang <bellehee98@naver.com>
2022-08-18 12:53:23 +0900

    22/07/20 숫자 맞추기

--- SLAcademy/SLAcademy/SL_Academy/SL_0720/src/Play.java
+++ SLAcademy/SLAcademy/SL_Academy/SL_0720/src/Play.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class Play {
+public class Play { //
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random rd = new Random();
