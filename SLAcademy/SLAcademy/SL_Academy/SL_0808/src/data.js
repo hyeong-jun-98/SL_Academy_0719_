6d693a4bc31029da944c6a30dc4a78515b8c4974
Yang <bellehee98@naver.com>
2022-08-18 13:33:50 +0900

    22/08/08 영화 포스터, 데이터 추출

--- SLAcademy/SLAcademy/SL_Academy/SL_0808/src/data.js
+++ SLAcademy/SLAcademy/SL_Academy/SL_0808/src/data.js
@@ -1,3 +1,4 @@
+//
 var movies = {
 	"marvel" : [
 		{
--- SLAcademy/SLAcademy/SL_Academy/SL_0808/src/practice.js
+++ SLAcademy/SLAcademy/SL_Academy/SL_0808/src/practice.js
@@ -1,3 +1,4 @@
+//
 var pet1 = {'name' : 'jindol', 'age' : '5', 'pets' : 'puppy'};
 var pet2 = {'name' : 'kong', 'age' : '3', 'pets' : 'cat'};
 var pet3 = {'name' : 'boli', 'age' : '2', 'pets' : 'lion'};
